d598dcbd842cfc45303322bfa596295fcde9fca1
Laura <eccentricporcupine@gmail.com>
2023-11-15 11:14:04 +0100

    don´t open

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -18,13 +18,36 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
+void copy_file(const std::string& fileName, const std::string& replaceName) {
+	std::string line;
+	std::ifstream infile(fileName);
+	std::ofstream outfile(replaceName);
+	if(!infile.is_open())
+		std::cout<<"can't open infile\n";
+	if(!outfile.is_open())
+		std::cout<<"can't open outfile\n";
+	if (infile && outfile) {
+		while (std::getline(infile, line)) {
+			outfile << line << "\n";
+		}
+	}
+	else
+		std::cout<<"Can't read\n";
+	infile.close();
+	outfile.close();
+}
 
 int main (int argc, char **argv) {
-
 	if (argc != 4)
 	{
 		std::cout<<"Wrong amount of arguments\n";
 		return 1;
 	}
-
+	std::string fileName = argv[1];
+	std::string replaceName = argv[1];
+	replaceName.append(".replace");
+	copy_file(fileName, replaceName);
+	return 0;
 }
--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -33,5 +33,5 @@ void Harl::warning() {
 }
 
 void Harl::complain(std::string level) {
-
+	
 }
